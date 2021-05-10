03eed3a5013168b30e5762b6d4ab4f1f6ead93c4
nkokor1 <nkokor1@etf.unsa.ba>
2021-05-10 23:47:40 +0200

    dodane klase Dvorana i Sjediste

---
+++ Aplikacija 1/Aplikacija 1/Models/Dvorana.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplikacija_1.Models
+{
+    public class Dvorana
+    {
+        #region Properties
+        [Key]
+        [Required]
+        public string Id { get => id; }
+        [Required]
+        public int BrojSjedista { get => brojSjedista; }
+        [NotMapped]
+        public List<Sjediste> Sjedista { get => sjedista; }
+        #endregion
+
+    }
+}
---
+++ Aplikacija 1/Aplikacija 1/Models/Sjediste.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplikacija_1.Models
+{
+    public class Sjediste
+    {
+        #region Properties
+        [Key]
+        [Required]
+        public string Id { get => id; }
+        [Required]
+        public bool JeLiSlobodno { get => jeLiSlobodno; set => jeLiSlobodno = value; }
+        #endregion
+
+    }
+}
