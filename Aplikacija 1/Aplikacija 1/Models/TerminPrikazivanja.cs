fb3d185d37c9c68d1c5edb21e4e0afbe271e74df
nkokor1 <nkokor1@etf.unsa.ba>
2021-05-10 23:38:31 +0200

    dodana klasa TerminPrikazivanja

---
+++ Aplikacija 1/Aplikacija 1/Models/TerminPrikazivanja.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplikacija_1.Models
+{
+    public class TerminPrikazivanja
+    {
+        #region Properties
+        [Key]
+        [Required]
+        public string Id { get => id; }
+        [Required]
+        public DanUSedmici Dan { get => dan; set => dan = value; }
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime DatumIVrijeme { get => datumIVrijeme; set => datumIVrijeme = value; }
+        #endregion
+
+    }
+}
