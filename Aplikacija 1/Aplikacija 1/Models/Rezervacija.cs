a8afb5d9d55fec355416cdc049274f8edf0a5dea
nkokor1 <nkokor1@etf.unsa.ba>
2021-05-10 23:44:26 +0200

    dodana klasa Rezervacija

---
+++ Aplikacija 1/Aplikacija 1/Models/Rezervacija.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplikacija_1.Models
+{
+    public class Rezervacija
+    {
+        #region properties
+        [Key]
+        [Required]
+        public string Id { get => id; }
+        [Required]
+        public string Korisnik { get => korisnik; }
+        [Required]
+        public double Cijena { get => cijena; }
+        [NotMapped]
+        public List<Ulaznica> Ulaznice { get => ulaznice; }
+        #endregion
+
+    }
+}
