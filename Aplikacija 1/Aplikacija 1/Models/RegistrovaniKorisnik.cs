e5cbac05151d5762006b3a5a9810a1e007a2048e
nkokor1 <nkokor1@etf.unsa.ba>
2021-05-10 23:49:16 +0200

    dodana klasa RegistrovaniKorisnik

---
+++ Aplikacija 1/Aplikacija 1/Models/RegistrovaniKorisnik.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplikacija_1.Models
+{
+    public class RegistrovaniKorisnik
+    {
+        #region Properties
+        [Key]
+        [Required]
+        public string Username { get => username; set => username = value; }
+        [Required]
+        public string Password { get => password; set => password = value; }
+        [Required]
+        public string Ime { get => ime; set => ime = value; }
+        [Required]
+        public string Prezime { get => prezime; set => prezime = value; }
+        [Required]
+        public string Email { get => email; set => email = value; }
+        public string ProfilnaFotografija { get => profilnaFotografija; set => profilnaFotografija = value; }
+        [Required]
+        public bool JeLiAdmin { get => jeLiAdmin; set => jeLiAdmin = value; }
+        #endregion
+
+    }
+}
