93e8c9c4b4ba550a6aa22d8dcde349fce136d3f3
nkokor1 <nkokor1@etf.unsa.ba>
2021-05-10 23:34:58 +0200

    dodana klasa film

---
+++ Aplikacija 1/Aplikacija 1/Models/Film.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplikacija_1.Models
+{
+    public class Film
+    {
+        #region Properties
+        [Key]
+        [Required]
+        public string Naziv { get => naziv; }
+        [Required]
+        public int Godina { get => godina; }
+        [Required]
+        public int Trajanje { get => trajanje; }
+        [NotMapped]
+        public List<string> Glumci { get => glumci; }
+        [NotMapped]
+        public List<string> Reziseri { get => reziseri; }
+        [NotMapped]
+        public List<TerminPrikazivanja> TerminiPrikazivanja { get => terminiPrikazivanja; set => terminiPrikazivanja = value; }
+        [Required]
+        public string Slika { get => slika; set => slika = value; }
+        [Required]
+        public string Trailer { get => trailer; }
+        public List<Zanr> Zanrovi { get => zanrovi; }
+        [Required]
+        public double CijenaUlaznice { get => cijenaUlaznice; set => cijenaUlaznice = value; }
+        public double ProsjecnaOcjena { get => prosjecnaOcjena; set => prosjecnaOcjena = value; }
+        [NotMapped]
+        public List<int> Ocjene { get => ocjene; set => ocjene = value; }
+        [Required]
+        public string Opis { get => opis; set => opis = value; }
+        [NotMapped]
+        public List<string> Komentari { get => komentari; set => komentari = value; }
+        #endregion
+    }
+}
